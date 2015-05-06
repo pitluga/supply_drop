e741b5a5d845d174b81191a5ed565acdffc29b7f
Prachetas Prabhu <prachetas.github@gmail.com>
2015-05-06 18:33:55 +0000

    Changes .cap to .rake for Cap3 standardization and uses wildcard instead of hardcoding

--- lib/capistrano/supply_drop.rb
+++ lib/capistrano/supply_drop.rb
@@ -2,4 +2,4 @@ require 'capistrano/supply_drop/rsync'
 require 'capistrano/supply_drop/async_enumerable'
 require 'capistrano/supply_drop/thread_pool'
 require 'capistrano/supply_drop/util'
-load File.expand_path('../tasks/supply_drop.cap', __FILE__)
+load File.expand_path('../tasks/supply_drop.rake', __FILE__)
